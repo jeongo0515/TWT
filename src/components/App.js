82e637086e9beee1f5cef77761dc931ce8d35c3e
jeongoo0515 <jjunjeongoo@naver.com>
2022-10-24 02:24:14 +0900

    Auth-01

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import AppRouter from "./Router";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
      <AppRouter isLoggedIn={isLoggedIn} />
-     <fooster>&copy; Twitter {new Date().getFullYear()}</fooster>
+     
     </>
   );
 }
