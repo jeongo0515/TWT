179a58a67b3c386522b8c67410223c93b1d390f3
jeongoo0515 <jjunjeongoo@naver.com>
2022-10-25 00:59:24 +0900

    Email log in

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true); // 회원가입 여부
+    const [error, setError] = useState("");
     const onChange = (event) => {
       const {
         target: { name, value },
@@ -13,15 +16,28 @@ const Auth = () => {
         setPassword(value);
       }
     };
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
       event.preventDefault();
+      try {
+          let data;
+        if(newAccount){        // 회원가입
+          data = await authService.createUserWithEmailAndPassword(email, password);
+        } else {   // 로그인
+         data = await authService.signInWithEmailAndPassword(email, password);
+        }
+        console.log(data)
+      } catch (error) {
+        setError(error.message);
+      }
     };
+const toggleAccount = () => setNewAccount((prev) => !prev);
+
     return (
       <div>
         <form onSubmit={onSubmit}>
           <input
             name="email"
-            type="text"
+            type="email"
             placeholder="Email"
             required
             value={email}
@@ -35,8 +51,13 @@ const Auth = () => {
             value={password}
             onChange={onChange}
           />
-          <input type="submit" value="Log In" />
+          <input type="submit" value={newAccount ? "Create Account" : "Log In"} 
+          />
+          {error}
         </form>
+        <span onClick={toggleAccount}>
+          {newAccount ? "Sign In" : "Create Account"}
+        </span>
         <div>
           <button>Continue with Google</button>
           <button>Continue with Github</button>
